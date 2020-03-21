7033586f3a1f4ce1dde4a938998514d755456ebf
Elizabeth King <elizking.dev@gmail.com>
2020-03-21 12:31:20 -0600

    stashing changes

--- src/containers/contact/index.js
+++ src/containers/contact/index.js
@@ -4,66 +4,66 @@ import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
 
 
-// const ContactMe = () => (
-//   <div>
-//     <Navbar />
-//     <div className="contactForm">
-//       <form action='/contact' method='post'>
-//         <input type='text' id='name' name='name' placeholder='Name' />
-//         <input type='email' id='email' name='email' placeholder='Email' />
-//         <textarea id="subject" name="subject" placeholder="Your message..." />
-//         <button type='submit'>Send</button>
-//       </form>
-//     </div>
-//     <Footer />
-//   </div>
-// )
-
-export default class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.submitForm = this.submitForm.bind(this);
-    this.state = {
-      status: ""
-    };
-  }
-
-  render() {
-    const { status } = this.state;
-    return (
-      <form
-        onSubmit={this.submitForm}
-        action="https://formspree.io/xjvrnblp"
-        method="POST"
-      >
-        <label>Email:</label>
-        <input type="email" name="email" />
-        <label>Message:</label>
-        <input type="text" name="message" />
-        {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
-        {status === "ERROR" && <p>Ooops! There was an error.</p>}
+const ContactMe = () => (
+  <div>
+    <Navbar />
+    <div className="contactForm">
+      <form action='/contact' method='post'>
+        <input type='text' id='name' name='name' placeholder='Name' />
+        <input type='email' id='email' name='email' placeholder='Email' />
+        <textarea id="subject" name="subject" placeholder="Your message..." />
+        <button type='submit'>Send</button>
       </form>
-    );
-  }
+    </div>
+    <Footer />
+  </div>
+)
 
-  submitForm(ev) {
-    ev.preventDefault();
-    const form = ev.target;
-    const data = new FormData(form);
-    const xhr = new XMLHttpRequest();
-    xhr.open(form.method, form.action);
-    xhr.setRequestHeader("Accept", "application/json");
-    xhr.onreadystatechange = () => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) return;
-      if (xhr.status === 200) {
-        form.reset();
-        this.setState({ status: "SUCCESS" });
-      } else {
-        this.setState({ status: "ERROR" });
-      }
-    };
-    xhr.send(data);
-  }
-}
+// export default class MyForm extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.submitForm = this.submitForm.bind(this);
+//     this.state = {
+//       status: ""
+//     };
+//   }
 
-// export default ContactMe;
+//   render() {
+//     const { status } = this.state;
+//     return (
+//       <form
+//         onSubmit={this.submitForm}
+//         action="https://formspree.io/xjvrnblp"
+//         method="POST"
+//       >
+//         <label>Email:</label>
+//         <input type='email' id='email' name='email' placeholder='Email' />
+//         <label>Message:</label>
+//         <textarea id="subject" name="subject" placeholder="Your message..." />
+//         {status === "SUCCESS" ? <p>Thanks!</p> : <button>Submit</button>}
+//         {status === "ERROR" && <p>Ooops! There was an error.</p>}
+//       </form>
+//     );
+//   }
+
+//   submitForm(ev) {
+//     ev.preventDefault();
+//     const form = ev.target;
+//     const data = new FormData(form);
+//     const xhr = new XMLHttpRequest();
+//     xhr.open(form.method, form.action);
+//     xhr.setRequestHeader("Accept", "application/json");
+//     xhr.onreadystatechange = () => {
+//       if (xhr.readyState !== XMLHttpRequest.DONE) return;
+//       if (xhr.status === 200) {
+//         form.reset();
+//         this.setState({ status: "SUCCESS" });
+//       } else {
+//         this.setState({ status: "ERROR" });
+//       }
+//     };
+//     xhr.send(data);
+//   }
+// }
+
+export default ContactMe;
